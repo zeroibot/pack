7dd1f4a533955d85f13c93941c2de4bed4f7fedb
roidaradal <roidaradal@gmail.com>
2026-03-01 15:08:25 +0800

    Set methods

--- ds/set.go
+++ ds/set.go
@@ -4,6 +4,18 @@ type Set[T comparable] struct {
 	items Map[T, struct{}]
 }
 
+// NewEmptySet creates a new empty set
+func NewEmptySet[T comparable]() *Set[T] {
+	return &Set[T]{items: make(Map[T, struct{}])}
+}
+
+// NewSetFrom creates a new set from given items
+func NewSetFrom[T comparable](items List[T]) *Set[T] {
+	set := NewEmptySet[T]()
+	set.AddItems(items)
+	return set
+}
+
 // Len returns the Set size
 func (s *Set[T]) Len() int {
 	return s.items.Len()
@@ -27,4 +39,65 @@ func (s *Set[T]) Copy() *Set[T] {
 // Items returns the Set items, in arbitrary order
 func (s *Set[T]) Items() List[T] {
 	return s.items.Keys()
+}
+
+// Add adds an item to the set
+func (s *Set[T]) Add(item T) {
+	s.items[item] = struct{}{}
+}
+
+// AddItems adds items to the set
+func (s *Set[T]) AddItems(items List[T]) {
+	for _, item := range items {
+		s.Add(item)
+	}
+}
+
+// Has checks if set contains item
+func (s *Set[T]) Has(item T) bool {
+	_, hasItem := s.items[item]
+	return hasItem
+}
+
+// HasNo checks if set does not contain item
+func (s *Set[T]) HasNo(item T) bool {
+	return !s.Has(item)
+}
+
+// Delete deletes an item from the set
+func (s *Set[T]) Delete(item T) {
+	delete(s.items, item)
+}
+
+// Union computes the union of two sets
+func (s *Set[T]) Union(s2 *Set[T]) *Set[T] {
+	s3 := NewEmptySet[T]()
+	for _, items := range []Map[T, struct{}]{s.items, s2.items} {
+		for item := range items {
+			s3.Add(item)
+		}
+	}
+	return s3
+}
+
+// Intersection computes the intersection of two sets
+func (s *Set[T]) Intersection(s2 *Set[T]) *Set[T] {
+	s3 := NewEmptySet[T]()
+	for item := range s.items {
+		if s2.Has(item) {
+			s3.Add(item)
+		}
+	}
+	return s3
+}
+
+// Difference computes the difference of two sets
+func (s *Set[T]) Difference(s2 *Set[T]) *Set[T] {
+	s3 := NewEmptySet[T]()
+	for item := range s.items {
+		if s2.HasNo(item) {
+			s3.Add(item)
+		}
+	}
+	return s3
 }
