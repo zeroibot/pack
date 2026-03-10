e35a76660008e423e0f13e4bf8f273a7fcb2d6f6
roidaradal <johnroy.daradal@goodkredit.com>
2026-03-10 10:45:32 +0800

    Reversed Range

--- ds/range.go
+++ ds/range.go
@@ -8,18 +8,24 @@ import (
 
 // Range represents a range of Integers from [start, end)
 type Range[T number.Integer] struct {
-	start T
-	end   T
+	start      T
+	end        T
+	isReversed bool
 }
 
 // NewRange creates a new Range from [start, end)
 func NewRange[T number.Integer](start, end T) Range[T] {
-	return Range[T]{start, end}
+	isReversed := start > end
+	return Range[T]{start, end, isReversed}
 }
 
 // NewInclusiveRange creates a new Range from [first, last]
 func NewInclusiveRange[T number.Integer](first, last T) Range[T] {
-	return Range[T]{first, last + 1}
+	if first > last {
+		// Reversed Range
+		return Range[T]{first, last - 1, true}
+	}
+	return Range[T]{first, last + 1, false}
 }
 
 // Limits returns the start, end limits of the Range
@@ -27,8 +33,16 @@ func (r Range[T]) Limits() (start, end T) {
 	return r.start, r.end
 }
 
+// IsReversed checks if the Range is reversed
+func (r Range[T]) IsReversed() bool {
+	return r.isReversed
+}
+
 // Len returns the size of the Range
 func (r Range[T]) Len() int {
+	if r.isReversed {
+		return int(r.start - r.end)
+	}
 	return int(r.end - r.start)
 }
 
@@ -39,14 +53,23 @@ func (r Range[T]) Copy() Range[T] {
 
 // Has checks if Integer is included in the Range
 func (r Range[T]) Has(item T) bool {
+	if r.isReversed {
+		return r.start >= item && item > r.end
+	}
 	return r.start <= item && item < r.end
 }
 
 // ToSlice expands the range into a slice of Integers
 func (r Range[T]) ToSlice() []T {
 	items := make([]T, 0, r.Len())
-	for x := r.start; x < r.end; x++ {
-		items = append(items, x)
+	if r.isReversed {
+		for x := r.start; x > r.end; x-- {
+			items = append(items, x)
+		}
+	} else {
+		for x := r.start; x < r.end; x++ {
+			items = append(items, x)
+		}
 	}
 	return items
 }
@@ -54,8 +77,14 @@ func (r Range[T]) ToSlice() []T {
 // Sum computes the sum of the Range
 func (r Range[T]) Sum() T {
 	var total T = 0
-	for x := r.start; x < r.end; x++ {
-		total += x
+	if r.isReversed {
+		for x := r.start; x > r.end; x-- {
+			total += x
+		}
+	} else {
+		for x := r.start; x < r.end; x++ {
+			total += x
+		}
 	}
 	return total
 }
@@ -63,8 +92,14 @@ func (r Range[T]) Sum() T {
 // Product computes the product of the Range
 func (r Range[T]) Product() T {
 	var product T = 1
-	for x := r.start; x < r.end; x++ {
-		product *= x
+	if r.isReversed {
+		for x := r.start; x > r.end; x-- {
+			product *= x
+		}
+	} else {
+		for x := r.start; x < r.end; x++ {
+			product *= x
+		}
 	}
 	return product
 }
--- ds/range_test.go
+++ ds/range_test.go
@@ -26,6 +26,11 @@ func TestRange(t *testing.T) {
 	if a != 1 || b != 11 {
 		t.Errorf("Range.Limits() = %d, %d, want = %d, %d", a, b, 11, 0)
 	}
+	for _, isReversed := range []bool{r1.IsReversed(), r2.IsReversed()} {
+		if isReversed != false {
+			t.Errorf("Range.IsReversed() = %t, want false", isReversed)
+		}
+	}
 	size := r1.Len()
 	if size != 5 {
 		t.Errorf("Range.Len() = %d, want = %d", size, 5)
@@ -86,6 +91,87 @@ func TestRange(t *testing.T) {
 	sliceCases := []Pair[[]int]{
 		{r1.ToSlice(), []int{0, 1, 2, 3, 4}},
 		{r2.ToSlice(), []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
+	}
+	for _, x := range sliceCases {
+		items, want := x.Values()
+		if reflect.DeepEqual(items, want) == false {
+			t.Errorf("Range.ToSlice() = %v, want = %v", items, want)
+		}
+	}
+}
+
+func TestReverseRange(t *testing.T) {
+	r1 := NewRange(5, 0)
+	r2 := NewInclusiveRange(10, 1)
+	textCases := []Pair[string]{
+		{r1.String(), "[5, 0)"},
+		{r2.String(), "[10, 0)"},
+	}
+	for _, x := range textCases {
+		text, want := x.Values()
+		if text != want {
+			t.Errorf("Range.String() = %q, want %q", text, want)
+		}
+	}
+	a, b := r1.Limits()
+	if a != 5 || b != 0 {
+		t.Errorf("Range.Limits() = %d, %d, want = %d, %d", a, b, 5, 0)
+	}
+	a, b = r2.Limits()
+	if a != 10 || b != 0 {
+		t.Errorf("Range.Limits() = %d, %d, want = %d, %d", a, b, 10, 0)
+	}
+	for _, isReversed := range []bool{r1.IsReversed(), r2.IsReversed()} {
+		if isReversed != true {
+			t.Errorf("Range.IsReversed() = %t, want true", isReversed)
+		}
+	}
+	size := r1.Len()
+	if size != 5 {
+		t.Errorf("Range.Len() = %d, want = %d", size, 5)
+	}
+	size = r2.Len()
+	if size != 10 {
+		t.Errorf("Range.Len() = %d, want = %d", size, 10)
+	}
+	r3 := r2.Copy()
+	a, b = r3.Limits()
+	if a != 10 || b != 0 {
+		t.Errorf("Range.Copy.Limits() = %d, %d, want = %d, %d", a, b, 10, 0)
+	}
+	hasCases := []Tuple2[int, bool]{
+		{3, true},
+		{0, false},
+		{-1, false},
+		{1, true},
+		{5, true},
+		{9, true},
+		{10, true},
+		{11, false},
+		{99, false},
+	}
+	for _, x := range hasCases {
+		item, want := x.Values()
+		actual := r2.Has(item)
+		if actual != want {
+			t.Errorf("Range.Has(%d) = %v, want = %v", item, actual, want)
+		}
+	}
+	testCases := []Tuple3[int, int, string]{
+		{r1.Sum(), 15, "Sum"},
+		{r2.Sum(), 55, "Sum"},
+		{r1.Product(), 120, "Product"},
+		{r2.Product(), 3628800, "Product"},
+	}
+	for _, x := range testCases {
+		actual, want, name := x.Values()
+		if actual != want {
+			t.Errorf("Range.%s() = %d, want = %d", name, actual, want)
+		}
+	}
+	sliceCases := []Pair[[]int]{
+		{r1.ToSlice(), []int{5, 4, 3, 2, 1}},
+		{r2.ToSlice(), []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}},
 	}
 	for _, x := range sliceCases {
 		items, want := x.Values()
