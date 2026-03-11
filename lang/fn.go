f64aa8b554164550cea4fa7d2dbae57ff794c2fc
roidaradal <roidaradal@gmail.com>
2026-03-11 21:27:35 +0800

    Equality functions

--- lang/fn.go
+++ lang/fn.go
@@ -1,18 +1,50 @@
 package lang
 
+import "cmp"
+
 // Identity returns the passed argument, used as function argument
 func Identity[T any](item T) T {
 	return item
 }
 
 // IsEqual returns a function that checks if value is equal to given value, used as function argument
+func IsEqual[T comparable](value T) func(T) bool {
+	return func(item T) bool {
+		return item == value
+	}
+}
 
 // NotEqual returns a function that checks if value is not equal to given value, used as function argument
+func NotEqual[T comparable](value T) func(T) bool {
+	return func(item T) bool {
+		return item != value
+	}
+}
 
-// IsGreater
+// IsGreater returns a function that checks if value is greater than given value, used as function argument
+func IsGreater[T cmp.Ordered](value T) func(T) bool {
+	return func(item T) bool {
+		return item > value
+	}
+}
 
-// IsGreaterEqual
+// IsGreaterEqual returns a function that checks if value is greater than or equal to given value, used as function argument
+func IsGreaterEqual[T cmp.Ordered](value T) func(T) bool {
+	return func(item T) bool {
+		return item >= value
+	}
+}
 
-// IsLesser
+// IsLesser returns a function that checks if value is lesser than given value, used as function argument
+func IsLesser[T cmp.Ordered](value T) func(T) bool {
+	return func(item T) bool {
+		return item < value
+	}
+}
 
-// IsLesserEqual
+// IsLesserEqual returns a function that checks if value is lesser than or equal to given value, used as function argument
+func IsLesserEqual[T cmp.Ordered](value T) func(T) bool {
+	return func(item T) bool {
+		return item <= value
+	}
+}
--- lang/fn_test.go
+++ lang/fn_test.go
@@ -14,3 +14,97 @@ func TestIdentity(t *testing.T) {
 		t.Errorf("Identity(%q) = %q, want %q", want2, actual2, want2)
 	}
 }
+
+func TestIsEqual(t *testing.T) {
+	fn1 := IsEqual(5)
+	fn2 := IsEqual("a")
+	testCases := [][2]bool{
+		{fn1(5), true},
+		{fn2("a"), true},
+		{fn1(6), false},
+		{fn2("b"), false},
+	}
+	for _, x := range testCases {
+		actual, want := x[0], x[1]
+		if actual != want {
+			t.Errorf("IsEqual = %v, want %v", actual, want)
+		}
+	}
+}
+
+func TestNotEqual(t *testing.T) {
+	fn1 := NotEqual(5)
+	fn2 := NotEqual("a")
+	testCases := [][2]bool{
+		{fn1(5), false},
+		{fn2("a"), false},
+		{fn1(6), true},
+		{fn2("b"), true},
+	}
+	for _, x := range testCases {
+		actual, want := x[0], x[1]
+		if actual != want {
+			t.Errorf("NotEqual = %v, want %v", actual, want)
+		}
+	}
+}
+
+func TestIsGreater(t *testing.T) {
+	fn := IsGreater(5)
+	testCases := [][2]bool{
+		{fn(4), false},
+		{fn(5), false},
+		{fn(6), true},
+	}
+	for _, x := range testCases {
+		actual, want := x[0], x[1]
+		if actual != want {
+			t.Errorf("IsGreater = %v, want %v", actual, want)
+		}
+	}
+}
+
+func TestIsGreaterEqual(t *testing.T) {
+	fn := IsGreaterEqual(5)
+	testCases := [][2]bool{
+		{fn(4), false},
+		{fn(5), true},
+		{fn(6), true},
+	}
+	for _, x := range testCases {
+		actual, want := x[0], x[1]
+		if actual != want {
+			t.Errorf("IsGreaterEqual = %v, want %v", actual, want)
+		}
+	}
+}
+
+func TestIsLesser(t *testing.T) {
+	fn := IsLesser(5)
+	testCases := [][2]bool{
+		{fn(4), true},
+		{fn(5), false},
+		{fn(6), false},
+	}
+	for _, x := range testCases {
+		actual, want := x[0], x[1]
+		if actual != want {
+			t.Errorf("IsGreater = %v, want %v", actual, want)
+		}
+	}
+}
+
+func TestIsLesserEqual(t *testing.T) {
+	fn := IsLesserEqual(5)
+	testCases := [][2]bool{
+		{fn(4), true},
+		{fn(5), true},
+		{fn(6), false},
+	}
+	for _, x := range testCases {
+		actual, want := x[0], x[1]
+		if actual != want {
+			t.Errorf("IsGreater = %v, want %v", actual, want)
+		}
+	}
+}
