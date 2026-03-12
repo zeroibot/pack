4547f5b1b12a4563d25f52be0c0c701da80f99cf
roidaradal <roidaradal@gmail.com>
2026-03-12 21:24:29 +0800

    Test list type

--- ds/list_test.go
+++ ds/list_test.go
@@ -1,12 +1,48 @@
 package ds
 
-import "testing"
+import (
+	"fmt"
+	"slices"
+	"testing"
+)
 
 func TestList(t *testing.T) {
-	// TODO: NewList
-	// TODO: Len, Cap, LastIndex
-	// TODO: IsEmpty, NotEmpty
-	// TODO: Copy
+	l1 := NewList[int](5)
+	l2 := List[string]{"a", "b", "c"}
+	testCases := []Tuple3[string, int, int]{
+		{"Len", l1.Len(), 0},
+		{"Cap", l1.Cap(), 5},
+		{"LastIndex", l1.LastIndex(), -1},
+		{"Len", l2.Len(), 3},
+		{"Cap", l2.Cap(), 3},
+		{"LastIndex", l2.LastIndex(), 2},
+	}
+	for _, x := range testCases {
+		name, actual, want := x.Values()
+		if actual != want {
+			t.Errorf("List.%s = %d, want %d", name, actual, want)
+		}
+	}
+	if l1.IsEmpty() != true {
+		t.Errorf("List.Empty = %v, want true", l1.IsEmpty())
+	}
+	if l2.NotEmpty() != true {
+		t.Errorf("List.NotEmpty = %v, want true", l2.NotEmpty())
+	}
+	l3 := l2.Copy()
+	if slices.Equal(l2, l3) == false {
+		t.Errorf("List.Copy() = %v, want %v", l3, l2)
+	}
+	l2[0] = "x"
+	l3[1] = "r"
+	actual, want := fmt.Sprintf("%v", l2), "[x b c]"
+	if actual != want {
+		t.Errorf("List.String() = %s, want %s", actual, want)
+	}
+	actual, want = fmt.Sprintf("%v", l3), "[a r c]"
+	if actual != want {
+		t.Errorf("List.String() = %s, want %s", actual, want)
+	}
 }
 
 func TestListMethods(t *testing.T) {
