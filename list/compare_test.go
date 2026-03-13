3e230b5e97923a4f0c403a87a25de19fee1dacfb
roidaradal <johnroy.daradal@goodkredit.com>
2026-03-13 10:40:20 +0800

    Test list all and any functions

--- list/compare_test.go
+++ list/compare_test.go
@@ -2,15 +2,114 @@ package list
 
 import "testing"
 
+func TestCompareAllAny(t *testing.T) {
+	var ints0 []int
+	ints1 := []int{1, 1, 1, 1}
+	ints2 := []int{1, 2, 3, 4}
+	ints3 := []int{2, 2, 1, 2}
+	var bools0 []bool
+	bools1 := []bool{true, true, true}
+	bools2 := []bool{false, false}
+	bools3 := []bool{false, true, false}
+	// AllEqual
+	actual := AllEqual(ints0, 5)
+	if actual != false {
+		t.Errorf("AllEqual() = %t, want %t", actual, false)
+	}
+	actual = AllEqual(ints1, 1)
+	if actual != true {
+		t.Errorf("AllEqual() = %t, want %t", actual, true)
+	}
+	actual = AllEqual(ints2, 3)
+	if actual != false {
+		t.Errorf("AllEqual() = %t, want %t", actual, false)
+	}
+	actual = AllEqual(ints3, 2)
+	if actual != false {
+		t.Errorf("AllEqual() = %t, want %t", actual, false)
+	}
+	// AllTrue, All False
+	actual = AllTrue(bools0)
+	if actual != false {
+		t.Errorf("AllTrue() = %t, want %t", actual, false)
+	}
+	actual = AllFalse(bools0)
+	if actual != false {
+		t.Errorf("AllFalse() = %t, want %t", actual, false)
+	}
+	actual = AllTrue(bools1)
+	if actual != true {
+		t.Errorf("AllTrue() = %t, want %t", actual, true)
+	}
+	actual = AllTrue(bools3)
+	if actual != false {
+		t.Errorf("AllTrue() = %t, want %t", actual, false)
+	}
+	actual = AllFalse(bools2)
+	if actual != true {
+		t.Errorf("AllFalse() = %t, want %t", actual, true)
+	}
+	actual = AllFalse(bools3)
+	if actual != false {
+		t.Errorf("AllFalse() = %t, want %t", actual, false)
+	}
+	// Has, HasNo
+	actual, actual2 := Has(ints0, 1), HasNo(ints0, 1)
+	if actual != false {
+		t.Errorf("Has() = %t, want %t", actual, false)
+	}
+	if actual2 != true {
+		t.Errorf("HasNo() = %t, want %t", actual2, true)
+	}
+	actual, actual2 = Has(ints1, 1), HasNo(ints1, 1)
+	if actual != true {
+		t.Errorf("Has() = %t, want %t", actual, true)
+	}
+	if actual2 != false {
+		t.Errorf("HasNo() = %t, want %t", actual2, false)
+	}
+	actual, actual2 = Has(ints3, 1), HasNo(ints3, 1)
+	if actual != true {
+		t.Errorf("Has() = %t, want %t", actual, true)
+	}
+	if actual2 != false {
+		t.Errorf("HasNo() = %t, want %t", actual2, false)
+	}
+	actual, actual2 = Has(ints2, 5), HasNo(ints2, 5)
+	if actual != false {
+		t.Errorf("Has() = %t, want %t", actual, false)
+	}
+	if actual2 != true {
+		t.Errorf("HasNo() = %t, want %t", actual2, true)
+	}
+	actual, actual2 = AnyTrue(bools1), AnyFalse(bools1)
+	if actual != true {
+		t.Errorf("AnyTrue() = %t, want %t", actual, true)
+	}
+	if actual2 != false {
+		t.Errorf("AnyFalse() = %t, want %t", actual2, false)
+	}
+	actual, actual2 = AnyTrue(bools2), AnyFalse(bools2)
+	if actual != false {
+		t.Errorf("AnyTrue() = %t, want %t", actual, false)
+	}
+	if actual2 != true {
+		t.Errorf("AnyFalse() = %t, want %t", actual2, true)
+	}
+	actual, actual2 = AnyTrue(bools3), AnyFalse(bools3)
+	if actual != true {
+		t.Errorf("AnyTrue() = %t, want %t", actual, true)
+	}
+	if actual2 != true {
+		t.Errorf("AnyFalse() = %t, want %t", actual2, true)
+	}
+}
+
 func TestListCompareQueries(t *testing.T) {
 	// TODO: IndexLookup
 	// TODO: IndexOf, AllIndexOf
 	// TODO: Remove, RemoveAll
-	// TODO: Has, HasNo
 	// TODO: GetOrDefault
-	// TODO: AllEqual
-	// TODO: AllTrue, AllFalse
-	// TODO: AnyTrue, AnyFalse
 }
 
 func TestListCompareMethods(t *testing.T) {
