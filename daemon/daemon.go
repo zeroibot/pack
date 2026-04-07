ad37d303a97e283a28c3ce81d065d81c13bc59e5
zeroibot <roidaradal@gmail.com>
2026-04-07 16:19:19 +0800

    Daemon Instance

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1,2 +1,23 @@
 // Package daemon contains daemon types and functions
 package daemon
+
+import (
+	"time"
+
+	"github.com/zeroibot/pack/dict"
+)
+
+type Instance struct {
+	start    *dict.SyncMap[string, time.Time]
+	last     *dict.SyncMap[string, time.Time]
+	duration *dict.SyncMap[string, time.Duration]
+}
+
+// NewInstance creates a new Daemon Instance
+func NewInstance() *Instance {
+	return new(Instance{
+		start:    dict.NewSyncMap[string, time.Time](),
+		last:     dict.NewSyncMap[string, time.Time](),
+		duration: dict.NewSyncMap[string, time.Duration](),
+	})
+}
